32efaef129e391dd12c63f45c5e67a90a1b925c4
manishch1095 <manishch1095@gmail.com>
2018-03-08 00:51:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,9 @@ var marginLeft=0;
 function moveRight ()
 {
     marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft;
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight,100);
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,50);
 };
 
