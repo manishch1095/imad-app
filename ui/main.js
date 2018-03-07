a1d937390694119cf1e680d6d7877ddc416d7d4d
manishch1095 <manishch1095@gmail.com>
2018-03-08 00:27:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element=document.getElementById('main-text');
 element.innerHTML="New Value";  
 
 var img=document.getElementById("madi");
-madi.onClick=function(){
-    madi.style.marginleft="100px";
+img.onClick=function(){
+    img.style.marginleft="100px";
     
 };
