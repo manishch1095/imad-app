03d734901ffe75d29d8b8cbf77d789c57bdc23ab
manishch1095 <manishch1095@gmail.com>
2018-04-05 16:27:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,31 @@
-console.log('Loaded!');
-//Change the text of the main-text div
-var element=document.getElementById('main-text');
+var button=document.getElementById('counter');
 
-element.innerHTML="New Value";  
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight ()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+button.onclick=function(){
+    
+    //create a request object
+    
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    
+    request.onreadystatechange=function(){
+        
+        if(request.readyState===XMLHttprequest.DONE){
+            
+            //take some action
+            if(request.readyState===200){
+                
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+        //not done yet
+    };
+    
+    //make the request
+    request.open('GET','http://manishch1095.imad.hasura-app.io/')
 };
+
 
